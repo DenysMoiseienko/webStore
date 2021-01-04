53565634855aa98f6a8c0bfcd0a6329a8feb02f2
Mariia <mariia.moiseienko1@gmail.com>
2021-01-04 19:11:33 +0100

    change the myaccount view

--- app/views/user/myaccount.php
+++ app/views/user/myaccount.php
@@ -9,23 +9,21 @@
 </div>
 <!--end-breadcrumbs-->
 
-<div class="container">
+<div class="container py-3">
     <div class="col-md-12">
         <h2>My account</h2>
-        <div>
-            <p>Update your information</p>
-            <a href="user/edit" class="btn btn-primary">Update my info</a>
-            <hr>
+        <hr>
+        <div class="my-2">
+            <span class="pt-2">You can update your information: </span><br>
+            <a href="user/edit" class="btn custom-btn-secondary">Update my info</a>
         </div>
-
-        <div>
-            <p>View and search your order history</p>
-            <a href="user/orders" class="btn btn-primary">Orders history</a>
-            <hr>
+        <div class="my-2">
+            <span class="pt-2">You can view your order history or find your order: </span><br>
+            <a href="user/orders" class="btn custom-btn-secondary">Orders history</a>
         </div>
-        <div>
+        <hr>
+        <div class="my-4">
             <a href="user/logout" class="btn btn-danger">Logout</a>
-            <hr>
         </div>
     </div>
 </div>
