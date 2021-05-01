0e7281cae7fc4a2ff1c8dde6cba5893fe0ae1967
Denys_Moiseienko <denys.moiseienko1@gmail.com>
2021-05-01 19:35:56 +0200

    remove unnecessary plugins for adminLTE

--- app/views/layouts/admin.php
+++ app/views/layouts/admin.php
@@ -7,31 +7,13 @@
     <base href="<?=PATH;?>">
     <?=$this->getMeta();?>
 
-    <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Font Awesome -->
     <link rel="stylesheet" href="adminLTE/plugins/fontawesome-free/css/all.min.css">
-    <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Tempusdominus Bbootstrap 4 -->
-    <link rel="stylesheet" href="adminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="adminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="adminLTE/plugins/jqvmap/jqvmap.min.css">
-    <!-- Theme style -->
     <link rel="stylesheet" href="adminLTE/dist/css/adminlte.min.css">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="adminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="adminLTE/plugins/daterangepicker/daterangepicker.css">
-    <!-- summernote -->
     <link rel="stylesheet" href="adminLTE/plugins/summernote/summernote-bs4.css">
-    <!-- Select 2 -->
     <link rel="stylesheet" href="adminLTE/plugins/select2/css/select2.css">
-    <!-- Google Font: Source Sans Pro-->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-
     <link rel="stylesheet" href="adminLTE/my.css">
 
 </head>
@@ -224,41 +206,18 @@
             adminPath = '<?=ADMIN;?>';
     </script>
 
-    <!-- jQuery -->
     <script src="adminLTE/plugins/jquery/jquery.min.js"></script>
-    <!-- jQuery UI 1.11.4 -->
     <script src="adminLTE/plugins/jquery-ui/jquery-ui.min.js"></script>
-    <script src="libs/ajaxupload.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
         $.widget.bridge('uibutton', $.ui.button)
     </script>
-    <!-- Bootstrap 4 -->
     <script src="adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- Validator -->
-    <script src="libs/validator.js"></script>
-    <!-- ChartJS -->
-    <script src="adminLTE/plugins/chart.js/Chart.min.js"></script>
-    <!-- Sparkline -->
-    <script src="adminLTE/plugins/sparklines/sparkline.js"></script>
-    <!-- JQVMap -->
-    <script src="adminLTE/plugins/jqvmap/jquery.vmap.min.js"></script>
-    <!--<script src="adminLTE/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>-->
-    <!-- jQuery Knob Chart -->
-    <script src="adminLTE/plugins/jquery-knob/jquery.knob.min.js"></script>
-    <!-- daterangepicker -->
-    <script src="adminLTE/plugins/moment/moment.min.js"></script>
-    <script src="adminLTE/plugins/daterangepicker/daterangepicker.js"></script>
-    <!-- Tempusdominus Bootstrap 4 -->
-    <script src="adminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-    <!-- Summernote -->
     <script src="adminLTE/plugins/summernote/summernote-bs4.min.js"></script>
-    <!-- Select 2 -->
     <script src="adminLTE/plugins/select2/js/select2.full.js"></script>
-    <!---->
-    <script src="adminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-    <!-- AdminLTE App -->
     <script src="adminLTE/dist/js/adminlte.js"></script>
+    <script src="libs/ajaxupload.js"></script>
+    <script src="libs/validator.js"></script>
     <script src="libs/typeahead.bundle.js"></script>
     <script src="adminLTE/my.js"></script>
 
